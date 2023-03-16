909ca63341b50167ed8eec8dd8804499b4ebd828
Abhishek23232 <127471560+Abhishek23232@users.noreply.github.com>
2023-03-16 19:40:33 +0530

    Add files via upload

---
+++ Helloa.java
@@ -0,0 +1,12 @@
+class Helloa{
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		System.out.println("Name:Abhishek Vidap");
+		System.out.println("Collage:A g patil");
+		System.out.println("I am from solapur");
+		System.out.println("Welcome To Anudip Foundation");
+		
+		
+	}
+}
